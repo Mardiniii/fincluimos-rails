7b2f998079629bedbb4e804cebd1d2f07bf0731a
mardiniii <sezama03@hotmail.com>
2016-04-09 01:24:03 -0500

    Add new info_collector view

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,5 +1,8 @@
 module Admin
   class AdminController < ApplicationController
+    before_action :authenticate_user!
 
+    def new_info_collector
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
 
+  namespace :admin do
+    get 'new_info_collector' => 'admin#new_info_collector'
+  end
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
