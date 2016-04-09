ebcb5fcf1adf11af0c7d8281b7b027153c8516d7
mardiniii <sezama03@hotmail.com>
2016-04-08 23:56:57 -0500

    Create user model with devise gem

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: companies
+#
+#  id           :integer          not null, primary key
+#  name         :string
+#  company_type :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Company < ActiveRecord::Base
 end
