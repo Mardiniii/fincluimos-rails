1b3f6706cfd6e121a0b397c1df1b8f3912bd9105
alejoescobar <alejoescobac@gmail.com>
2016-04-10 08:28:45 -0500

    Add form responses

---
+++ app/controllers/admin/form_responses_controller.rb
@@ -0,0 +1,2 @@
+class Admin::FormResponsesController < ApplicationController
+end
---
+++ test/controllers/admin/form_responses_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admin::FormResponsesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
