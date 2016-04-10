76fcc64cf7315f953a5451acb63f46ee39b5cf5f
Simon S. Soriano <simon0191@gmail.com>
2016-04-10 02:59:02 -0500

    Add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,46 @@ if Rails.env.development?
   first_name_question = form.questions.find_or_create_by(text: "Nombres") { |q| q.question_type = :input }
   last_name_question = form.questions.find_or_create_by(text: "Apellidos") { |q| q.question_type = :input }
   department_question = form.questions.find_or_create_by(text: "Departamento") { |q| q.question_type = :dropdown }
-  department_question.question_options.find_or_create_by(text: "Cundinamarca")
-  department_question.question_options.find_or_create_by(text: "Boyaca")
-  department_question.question_options.find_or_create_by(text: "Meta")
-  department_question.question_options.find_or_create_by(text: "Antioquia")
-  department_question.question_options.find_or_create_by(text: "Nariño")
+    department_question.question_options.find_or_create_by(text: "Cundinamarca")
+    department_question.question_options.find_or_create_by(text: "Boyaca")
+    department_question.question_options.find_or_create_by(text: "Meta")
+    department_question.question_options.find_or_create_by(text: "Antioquia")
+    department_question.question_options.find_or_create_by(text: "Nariño")
+
+  form.questions.find_or_create_by(text: "Edad") { |q| q.question_type = :input }
+
+  q = form.questions.find_or_create_by(text: "Grupo etnico") { |q| q.question_type = :dropdown }
+    q.question_options.find_or_create_by(text: "Indigena")
+    q.question_options.find_or_create_by(text: "Mestizo")
+    q.question_options.find_or_create_by(text: "Caucasico")
+
+  q2 = form.questions.find_or_create_by(text: "Musica que le gusta") { |q| q.question_type = :checkbox }
+    q2.question_options.find_or_create_by(text: "Rock")
+    q2.question_options.find_or_create_by(text: "Salsa")
+    q2.question_options.find_or_create_by(text: "Merengue")
+
+  form = company.forms.find_or_create_by(name: "Registro de usuario")
+
+  first_name_question = form.questions.find_or_create_by(text: "Nombres") { |q| q.question_type = :input }
+  last_name_question = form.questions.find_or_create_by(text: "Apellidos") { |q| q.question_type = :input }
+  department_question = form.questions.find_or_create_by(text: "Departamento") { |q| q.question_type = :dropdown }
+    department_question.question_options.find_or_create_by(text: "Cundinamarca")
+    department_question.question_options.find_or_create_by(text: "Boyaca")
+    department_question.question_options.find_or_create_by(text: "Meta")
+    department_question.question_options.find_or_create_by(text: "Antioquia")
+    department_question.question_options.find_or_create_by(text: "Nariño")
+
+  form.questions.find_or_create_by(text: "Edad") { |q| q.question_type = :input }
+
+  q = form.questions.find_or_create_by(text: "Grupo etnico") { |q| q.question_type = :dropdown }
+    q.question_options.find_or_create_by(text: "Indigena")
+    q.question_options.find_or_create_by(text: "Mestizo")
+    q.question_options.find_or_create_by(text: "Caucasico")
+
+  q2 = form.questions.find_or_create_by(text: "Musica que le gusta") { |q| q.question_type = :checkbox }
+    q2.question_options.find_or_create_by(text: "Rock")
+    q2.question_options.find_or_create_by(text: "Salsa")
+    q2.question_options.find_or_create_by(text: "Merengue")
+
 
 end
